b5a5dc9aee59f1801ccff6fcf54d8bbec864c2e9
Daniel Gerson <dmg46664@Daniels-MacBook-Pro.local>
2023-09-28 15:26:35 +0100

    Few more fixes.

--- 0009_portfolio_skia_render/src-portfolio/main.cljs
+++ 0009_portfolio_skia_render/src-portfolio/main.cljs
@@ -3,32 +3,16 @@
             [reagent.core :as r]
             [uix.core :refer [$ defui]]
             [uix.dom]
-
             ["react-native" :as rn]
-
             [shadow.lazy :as lazy]
             [shadow.cljs.modern :refer [js-await]]
 
             ["@shopify/react-native-skia/lib/module/web" :refer [WithSkiaWeb]]
             ["canvaskit-wasm/package.json" :refer [version]]))
 
-;; See setting up skia web
-;; https://github.com/Shopify/react-native-skia/issues/927
-;; https://github.com/PEZ/rn-rf-shadow
-;; https://retool.com/blog/how-to-make-your-react-native-apps-work-on-the-web/
-;; https://necolas.github.io/react-native-web/docs/?path=%2Fdocs%2Foverview-getting-started--page
-;; https://necolas.github.io/react-native-web/docs/setup/
-
-;; Is the reason that it is looking into internals?
-;; https://github.com/necolas/react-native-web/issues/2147
-;; Can't be, or this wouldn't work with non clojure demos.
-
-
-;; TODO Problems https://github.com/Shopify/react-native-skia/issues/1871
-
 (defscene react-native
   (r/as-element
-   [:> rn/Text {} (str "Hello world: ")
+   [:> rn/TextASDFADS {} (str "Hello world!")
     ]
    ))
 
@@ -41,10 +25,6 @@
 (def loadable-canvas (lazy/loadable app/skia-canvas))
 
 (defscene skia-canvas
-  #_ 
-  (r/as-element
-   [:> rn/Text {} (str "Hello world: ")])
-
   ($ WithSkiaWeb
 
        {:opts #js {:locateFile
